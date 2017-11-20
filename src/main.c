55202d3af2f7d8b83d6a3d1bbd42f589392f9c18
Leonardo Foderaro <startrack79@gmail.com>
2017-11-20 11:16:03 +0100

    added HAMSTER_PORT  constant and hamster_listener composition

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-//  Hello World server, copied from http://zguide.zeromq.org/c:hwserver
+//  ZeroMQ-base Hamster server, largely copied from http://zguide.zeromq.org/c:hwserver
 
 #include <zmq.h>
 #include <stdio.h>
@@ -6,12 +6,23 @@
 #include <string.h>
 #include <assert.h>
 
+#define HAMSTER_PORT 8888
+
 int main (void)
 {
     //  Socket to talk to clients
     void *context = zmq_ctx_new ();
     void *responder = zmq_socket (context, ZMQ_REP);
-    int rc = zmq_bind (responder, "tcp://*:5555");
+
+    char listener_address[16];
+
+    memset(listener_address, 0, 16);
+
+    snprintf(listener_address, sizeof(listener_address), "tcp://*:%d", HAMSTER_PORT);
+
+    printf("Hamster is happily running and counting on port %d.\n", HAMSTER_PORT);
+
+    int rc = zmq_bind (responder, listener_address);
     assert (rc == 0);
 
     while (1) {
