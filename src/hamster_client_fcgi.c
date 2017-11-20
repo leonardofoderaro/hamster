a237e8066c19cedf3db9c2d5a35959c7e9f07909
Leonardo Foderaro <startrack79@gmail.com>
2017-11-20 14:01:10 +0100

    Hamster fcgi client now can talk to Hamster server using zero-mq

--- src/hamster_client_fcgi.c
+++ src/hamster_client_fcgi.c
@@ -5,12 +5,34 @@
 
 #include <fcgi_stdio.h>
 #include <stdlib.h>
+#include <zmq.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
 int main(void)
 {
     int count = 0;
-    while(FCGI_Accept() >= 0)
+
+    char buffer [10];
+
+    void *context = zmq_ctx_new ();
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://localhost:8888");
+
+    while(FCGI_Accept() >= 0) {
+        zmq_send (requester, "Hello", 5, 0);
+        zmq_recv (requester, buffer, 10, 0);
+
         printf("Content-type: text/html\r\n\r\n"
-               "Request number %d on host <i>%s</i>\n",
-               ++count, getenv("SERVER_NAME"));
+               "Hamster says %s on host <i>%s</i>\n",
+               buffer, getenv("SERVER_NAME"));
+
+    }
+
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+
     return 0;
 }
